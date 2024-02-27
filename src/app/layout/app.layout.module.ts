0988082e283506032d1a6f63b6cfe97f45cb9297
GoktugYmsk <gogtug033@outlook.com>
2024-02-27 23:26:53 +0300

    layout and menu components are cleared

--- src/app/layout/app.layout.module.ts
+++ src/app/layout/app.layout.module.ts
@@ -14,9 +14,8 @@ import { AppMenuitemComponent } from './app.menuitem.component';
 import { RouterModule } from '@angular/router';
 import { AppTopBarComponent } from './app.topbar.component';
 import { AppFooterComponent } from './app.footer.component';
-import { AppConfigModule } from './config/config.module';
-import { AppSidebarComponent } from "./app.sidebar.component";
-import { AppLayoutComponent } from "./app.layout.component";
+import { AppSidebarComponent } from './app.sidebar.component';
+import { AppLayoutComponent } from './app.layout.component';
 
 @NgModule({
     declarations: [
@@ -39,8 +38,7 @@ import { AppLayoutComponent } from "./app.layout.component";
         InputSwitchModule,
         RippleModule,
         RouterModule,
-        AppConfigModule
     ],
-    exports: [AppLayoutComponent]
+    exports: [AppLayoutComponent],
 })
-export class AppLayoutModule { }
+export class AppLayoutModule {}
