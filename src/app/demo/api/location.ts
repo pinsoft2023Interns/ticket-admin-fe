608f1a047ece5c5ef5a59b344ee3ddb1aa2b77c0
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-03-08 16:58:19 +0300

    bug fix

--- src/app/demo/api/location.ts
+++ src/app/demo/api/location.ts
@@ -1,5 +1,12 @@
+export interface District {
+    area?: number;
+    id?: number;
+    name?: string;
+    population?: number;
+}
 
 export interface Location {
     name: string;
     id: number;
+    districts: District[];
 }
