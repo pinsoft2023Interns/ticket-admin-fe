96b786d53af091164678ef59edb5c52f6d27bbf5
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-05-22 00:57:57 +0300

    bug fix

--- src/app/demo/components/auth/login/login.component.ts
+++ src/app/demo/components/auth/login/login.component.ts
@@ -1,5 +1,4 @@
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
 import { AuthGuard } from '../auth.guard';
@@ -10,17 +9,24 @@ import { MessageService } from 'primeng/api'; // Ekledim
     selector: 'app-login',
     templateUrl: './login.component.html',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
     username: string = '';
     password: string = '';
 
     constructor(
         public layoutService: LayoutService,
-        private http: HttpClient,
         private router: Router,
         private authGuard: AuthGuard,
         private authService: AuthService
     ) { }
+
+    ngOnInit() {
+        const authToken = localStorage.getItem('ticket-web-admin-authToken');
+        const userId = localStorage.getItem('ticket-web-admin-userId');
+        if (authToken && userId) {
+            this.router.navigate(['/company/dashboard']);
+        }
+    }
 
     login() {
         const trimmedUsername = this.username.trim();
